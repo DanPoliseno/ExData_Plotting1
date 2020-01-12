0f967abecf6def8a49d2d1d1656d4475c31a1a69
Dan Poliseno <dpolise@udel.edu>
2020-01-12 18:14:52 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -6,10 +6,8 @@ dtf <- read.table("household_power_consumption.txt", header = T,
 
 dtf[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
-# Change Date Column to Date Type
 dtf[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
-# Filter Dates for 2007-02-01 and 2007-02-02
 dtf <- dtf[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
