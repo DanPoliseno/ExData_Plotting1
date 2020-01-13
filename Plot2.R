61c74bd97bdf690ec745a1019f1f335eed1945ac
Dan Poliseno <dpolise@udel.edu>
2020-01-12 19:29:43 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -13,6 +13,8 @@ attach(dtf)
 
 dev.copy(png, file = "plot2.png", height = 480, width = 480)
 
+#Plot 2 
+
 plot(Global_active_power ~ dateTime, type = "l",
      ylab = "Global Active Power (Kilowatts)", xlab = "")
      
